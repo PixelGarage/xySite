a82d06a34847e581e81680499579691fdb2f80f6
Ralph Moser <ralph@ramosoft.ch>
2015-10-18 01:10:28 +0200

    - flexpaper2 module supports several fields on page and multiple files per field

--- all/modules/_pixelgarage/flexpaper2/js/flexpaper2.js
+++ all/modules/_pixelgarage/flexpaper2/js/flexpaper2.js
@@ -1,65 +1,78 @@
 (function ($) {
+
+  /**
+   * Initializes all flexpaper viewers.
+   */
   Drupal.behaviors.flexpaper2Viewer = {
     attach: function (context, settings) {
-      //Get paths for published files
-      var swfFiles       = Drupal.settings.flexpaper2.swfFiles;
-      var jsonFiles      = Drupal.settings.flexpaper2.jsonFiles;
-      var pdfFiles       = Drupal.settings.flexpaper2.pdfFiles;
-      var pngFiles       = Drupal.settings.flexpaper2.pngFiles;
-      var thumbFiles     = Drupal.settings.flexpaper2.thumbFiles;
-      var renderingOrder = Drupal.settings.flexpaper2.RenderingOrder;
+      // initialize Flexpaper viewer
+      var flexpaper2_init = function (element_id, index, settings) {
+        var elementSettings = settings[index];
 
-      var flexpaper2_init = function (element_id, index) {
         $('#' + element_id).FlexPaperViewer(
           {
             config: {
-              SWFFile: swfFiles[index],
-              JSONFile: jsonFiles[index],
-              IMGFiles: pngFiles[index],
-              PDFFile: pdfFiles[index],
-              ThumbIMGFiles: thumbFiles[index],
+              SWFFile: elementSettings.swfFiles,
+              JSONFile: elementSettings.jsonFiles,
+              IMGFiles: elementSettings.imgFiles,
+              PDFFile: elementSettings.pdfFiles,
+              ThumbIMGFiles: elementSettings.thumbFiles,
 
-              Scale: Drupal.settings.flexpaper2.Scale,
-              ZoomTransition: Drupal.settings.flexpaper2.ZoomTransition,
-              ZoomTime: Drupal.settings.flexpaper2.ZoomTime,
-              ZoomInterval: Drupal.settings.flexpaper2.ZoomInterval,
-              FitPageOnLoad: Drupal.settings.flexpaper2.FitPageOnLoad == 1,
-              FitWidthOnLoad: Drupal.settings.flexpaper2.FitWidthOnLoad == 1,
-              FullScreenAsMaxWindow: Drupal.settings.flexpaper2.FullScreenAsMaxWindow == 1,
-              ProgressiveLoading: Drupal.settings.flexpaper2.ProgressiveLoading == 1,
-              MinZoomSize: Drupal.settings.flexpaper2.MinZoomSize,
-              MaxZoomSize: Drupal.settings.flexpaper2.MaxZoomSize,
-              SearchMatchAll: Drupal.settings.flexpaper2.SearchMatchAll == 1,
-              InitViewMode: Drupal.settings.flexpaper2.InitViewMode,
-              RenderingOrder: renderingOrder,
+              Scale: parseFloat(elementSettings.Scale),
+              ZoomTransition: elementSettings.ZoomTransition,
+              ZoomTime: parseFloat(elementSettings.ZoomTime),
+              ZoomInterval: parseFloat(elementSettings.ZoomInterval),
+              FitPageOnLoad: elementSettings.FitPageOnLoad == 1,
+              FitWidthOnLoad: elementSettings.FitWidthOnLoad == 1,
+              FullScreenAsMaxWindow: elementSettings.FullScreenAsMaxWindow == 1,
+              ProgressiveLoading: elementSettings.ProgressiveLoading == 1,
+              MinZoomSize: parseFloat(elementSettings.MinZoomSize),
+              MaxZoomSize: parseFloat(elementSettings.MaxZoomSize),
+              SearchMatchAll: elementSettings.SearchMatchAll == 1,
+              InitViewMode: elementSettings.InitViewMode,
+              RenderingOrder: elementSettings.RenderingOrder,
 
-              jsDirectory: Drupal.settings.flexpaper2.jsDirectory,
-              cssDirectory: Drupal.settings.flexpaper2.cssDirectory,
-              localeDirectory: Drupal.settings.flexpaper2.localeDirectory,
+              ViewModeToolsVisible: elementSettings.ViewModeToolsVisible == 1,
+              ZoomToolsVisible: elementSettings.ZoomToolsVisible == 1,
+              NavToolsVisible: elementSettings.NavToolsVisible == 1,
+              CursorToolsVisible: elementSettings.CursorToolsVisible == 1,
+              SearchToolsVisible: elementSettings.SearchToolsVisible == 1,
+              UIConfig: elementSettings.UIConfig,
 
-              ViewModeToolsVisible: Drupal.settings.flexpaper2.ViewModeToolsVisible == 1,
-              ZoomToolsVisible: Drupal.settings.flexpaper2.ZoomToolsVisible == 1,
-              NavToolsVisible: Drupal.settings.flexpaper2.NavToolsVisible == 1,
-              CursorToolsVisible: Drupal.settings.flexpaper2.CursorToolsVisible == 1,
-              SearchToolsVisible: Drupal.settings.flexpaper2.SearchToolsVisible == 1,
+              jsDirectory: settings.jsDirectory,
+              cssDirectory: settings.cssDirectory,
+              localeDirectory: settings.localeDirectory,
 
-              UIConfig: Drupal.settings.flexpaper2.UIConfig,
+              StartAtPage : 1,
+              MixedMode : true,
+              EnableWebGL : true,
+              AutoAdjustPrintSize : true,
+              PrintPaperAsBitmap : false,
+              AutoDetectLinks :true,
+              PublicationTitle : '',
+
               localeChain: 'en_US',
+              WMode : 'transparent',
               JSONDataType: 'json',
-              key: Drupal.settings.flexpaper2.licenseKey
+              key: settings.licenseKey
             }
           }
         );
       };
 
-      var viewers = $('div.flexpaper_viewer');
+      // iterate over all flexpaper fields
+      $.each(Drupal.settings.flexpaper2, function (fieldName, settings) {
+        var fieldClass  = '.field-name-' + fieldName,
+            $viewers     = $(fieldClass + ' .flexpaper_viewer');
 
-      //Put ids for this elements, because flexpaper need it.
-      viewers.each(function (index) {
-        var elementId = 'documentViewer_' + index;
-        $(this).attr('id', elementId);
-        flexpaper2_init(elementId, index);
+        //Put ids on all viewers per field, because flexpaper needs it.
+        $viewers.each(function (index) {
+          var elementId = fieldName + '-documentViewer-' + index;
+          $(this).attr('id', elementId);
+          flexpaper2_init(elementId, index, settings);
+        });
       });
+
     }
   }
 })(jQuery);
