3953782071d2784e52bad3d7f59c8beee6b0dea6
Ralph Moser <ralph@ramosoft.ch>
2017-04-10 13:10:21 +0200

    - proximity linked click added

--- all/modules/_pixelgarage/proximity/js/proximity.non-ajax.js
+++ all/modules/_pixelgarage/proximity/js/proximity.non-ajax.js
@@ -1,5 +1,5 @@
 /**
- * Handles an full area item clickif AJAX is disabled.
+ * Handles an full area item click, if AJAX is disabled.
  *
  * Created on 11.12.15.
  */
@@ -14,11 +14,20 @@
       $.each(Drupal.settings.proximity, function (container, settings) {
 
         var $container = $('#' + container),
-            $items     = $container.find('.pe-item-ajax');
+            $items     = $container.find('.pe-item-ajax'),
+            $linkedItems = $container.find('.pe-item-linked');
 
         $items.once('click', function () {
           $(this).on('click', function () {
             window.location = $(this).find("a:first").attr("href");
+            return false;
+          });
+        });
+
+        $linkedItems.once('click', function () {
+          $(this).on('click', function () {
+            var link = $(this).find(".pe-item-inner > a").attr("href");
+            window.open(link, '_blank');
             return false;
           });
         });
